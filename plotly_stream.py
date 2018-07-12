cdfba0b792108a64a2b3d006c7878fd81b361dce
Michael Budnick <MichaelDBudnick@gmail.com>
2018-07-12 12:07:15 -0400

    Format y-axes

--- plotly_stream.py
+++ plotly_stream.py
@@ -7,21 +7,30 @@ def stream_data(datetimes, pH, temp, lux):
 	trace_ph = go.Scatter(
 		name='ph_readings',
 		x=datetimes,
-	    y=pH
+	    y=pH,
+		marker = dict(
+		 	color='#1f77b4'
+		)
 	)
 
 	trace_temp = go.Scatter(
 		name='temp_readings',
 		x=datetimes,
 	    y=temp,
-	    yaxis='y2'
+	    yaxis='y2',
+		marker = dict(
+		 	color='#ff7f0e'
+		)
 	)
 
 	trace_lux = go.Scatter(
 		name='lux_readings',
 		x=datetimes,
 	    y=lux,
-	    yaxis='y3'
+	    yaxis='y3',
+		marker = dict(
+		 	color='#2ca02c'
+		)
 	)
 
 	layout = go.Layout(
@@ -33,7 +42,9 @@ def stream_data(datetimes, pH, temp, lux):
 	        ),
         	tickfont=dict(
             	color='#1f77b4'
-        	)
+        	),
+			dtick=0.25,
+			range=[6.5, 7.5]
 	    ),
 	    yaxis2=dict(
 	        title='Celsius',
@@ -43,22 +54,26 @@ def stream_data(datetimes, pH, temp, lux):
 			tickfont=dict(
 		    	color='#ff7f0e'
 			),
+			dtick=0.5,
 			anchor='free',
 			overlaying='y',
 			side='left',
-			position=0.15
+			position=0.15,
+			range=[20, 29]
 		),
 		yaxis3=dict(
 	    	title='Lux',
 			    titlefont=dict(
-			    color='#d62728'
+			    color='#2ca02c'
 			),
 			tickfont=dict(
-			    color='#d62728'
+			    color='#2ca02c'
 			),
+			dtick=25,
 			anchor='x',
 			overlaying='y',
-			side='right'
+			side='right',
+			range=[0,225]
 		)
 	)
 
