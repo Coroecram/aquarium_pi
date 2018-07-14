e449a9c355118fa076d364424ab33522ae7a5bad
Michael Budnick <michaeldbudnick@gmail.com>
2018-07-14 09:56:41 -0400

    Rudientary plot

--- pg_read.r
+++ pg_read.r
@@ -22,22 +22,20 @@ dbExistsTable(con, "aquarium_data")
 # TRUE
 
 # query the data from postgreSQL
-df_aqua <- dbGetQuery(con, "SELECT * from aquarium_data LIMIT 15;")
+df_aqua <- dbGetQuery(con, "SELECT * from aquarium_data;")
 
 
 # colnames(df_aqua)
 head(df_aqua)
 df_aqua$observed_at
-df$timestamp = strptime(df$observed_at, "%Y-%m-%dT%H:%M:%S")
-df_aqua$ph_read
-df_aqua$temp_read
-df_aqua$lux_read
+# 2018-07-03 06:41:46
+# df_aqua$ph_read
+# df_aqua$temp_read
+# df_aqua$lux_read
 
 plot(df_aqua$observed_at, df_aqua$ph_read)
 scatter.smooth(x=df_aqua$observed_at, y=df_aqua$ph_read, main="Aquarium pH")
 
-
 # Basic Graph of the Data
-require(ggplot2)
-# ggplot(df_postgres, aes(x = as.factor(cyl), y = mpg, fill = as.factor(cyl))) +
-#   geom_boxplot() + theme_bw()
+# require(ggplot2)
+# ggplot(df_aqua, aes(x = observed_at, y = ph_read))
